2003fd74c6452cbd9e1d7b4a940e4e011e857dda
daiphat1808e <phat@sc0ille.com>
2017-08-28 10:36:50 +0900

    delete the backup of the previous version

--- frontend_backend copy/client/app.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import {render} from 'react-dom'
-import AppRouter from './AppRouter.js'
-
-render(<AppRouter/>, document.getElementById('root'))
--- frontend_backend copy/client/modules/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import isLogged from './isLogin';
-const rootReducer = combineReducers({
-  isLogged
-});
-export default rootReducer;
